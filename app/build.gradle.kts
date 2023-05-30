451f5292b378f25862d8b0ef99afcbc4b8aade1f
atomasg <abeltomas.plm@gmail.com>
2023-05-30 02:09:35 +0200

    Added necesary libraries and permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.gson)
+    implementation(libs.converter.gson)
+    implementation(libs.retrofit)
+    implementation(libs.loggerInterceptor)
+    implementation(libs.picasso)
 }
