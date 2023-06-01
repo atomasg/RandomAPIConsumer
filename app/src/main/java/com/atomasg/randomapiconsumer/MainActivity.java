eacac1fa5898a6fa8a0ff54d82c39f7741d5b0f1
atomasg <abeltomas.plm@gmail.com>
2023-06-01 15:18:37 +0200

    Added animation and navigations

--- app/src/main/java/com/atomasg/randomapiconsumer/MainActivity.java
+++ app/src/main/java/com/atomasg/randomapiconsumer/MainActivity.java
@@ -1,16 +1,27 @@
 package com.atomasg.randomapiconsumer;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.Manifest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.atomasg.randomapiconsumer.databinding.ActivityMainBinding;
-import com.atomasg.randomapiconsumer.ui.fragment.ContactListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private NavController navController;
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +29,17 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        this.navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        this.appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager
-                .beginTransaction()
-                .add(R.id.fragment_container, new ContactListFragment(), ContactListFragment.class.getSimpleName())
-                .commit();
+        // Set up ActionBar
+        setSupportActionBar(binding.toolbar);
+        NavigationUI.setupActionBarWithNavController(this, this.navController, this.appBarConfiguration);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
 
 
